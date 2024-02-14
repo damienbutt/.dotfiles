f346f89ff319c12b8cda74a66799d2be8508121a
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-02-14 22:31:32 +0000

    refactor(hyper): remove fig config

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -153,7 +153,7 @@ module.exports = {
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
-    localPlugins: ["fig-hyper-integration", "dracula-pro"],
+    localPlugins: [ "dracula-pro"],
     keymaps: {
         // Example
         // 'window:devtools': 'cmd+alt+o',
