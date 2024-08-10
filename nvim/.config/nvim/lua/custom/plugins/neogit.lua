23b5badb824344dc476309e4ce56638e48e1743f
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-08-10 14:50:37 +0100

    fix(nvim): update config

---
+++ nvim/.config/nvim/lua/custom/plugins/neogit.lua
@@ -0,0 +1,14 @@
+-- File: lua/custom/plugin/neogit.lua
+
+return {
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
+
+		-- Only one of these is needed, not both.
+		"nvim-telescope/telescope.nvim", -- optional
+		--"ibhagwan/fzf-lua",              -- optional
+	},
+	config = true,
+}
